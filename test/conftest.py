a5d18aba46fb3a67d741c276afdf39e713a04012
李文雅 <kiki@liwenyadeMacBook-Air.local>
2026-02-03 23:02:03 +0800

    chore: save local changes before pulling

--- test/conftest.py
+++ test/conftest.py
@@ -1,17 +1,20 @@
 import pytest
-import json
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 
 @pytest.fixture(scope="module")
 def shared_driver():
-    with open("config.json", "r", encoding="utf-8") as file:
-        config = json.load(file)
-    appium_server_url = config['appium_server_url']
-    capabilities = config['capabilities']
-    
+    appium_server_url = "http://localhost:4723"
+    capabilities = {
+        "platformName": "Android",            
+        "platformVersion": "14.0",            
+        "deviceName": "Pixel 9 Pro API 35",        
+        "browserName": "Chrome",              
+        "automationName": "UiAutomator2",     
+        "udid": "emulator-5554"
+    }
     options = UiAutomator2Options().load_capabilities(capabilities)
-    driver = webdriver.Remote(appium_server_url, options=options)
+    driver = webdriver.Remote(command_executor=appium_server_url, options=options)
     
     yield driver 
     driver.quit()
